f9774789d3fef492ef173416249b466b083231f7
Kubbi <nicoladelaroche@gmail.com>
2025-10-24 17:39:46 +0200

    Remove unnecessary mod entrypoint

--- src/main/java/me/kubbidev/legacychat/mixins/ToastManagerMixin.java
+++ src/main/java/me/kubbidev/legacychat/mixins/ToastManagerMixin.java
@@ -1,6 +1,7 @@
 package me.kubbidev.legacychat.mixins;
 
-import me.kubbidev.legacychat.LegacyChatMod;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.toast.SystemToast;
 import net.minecraft.client.toast.Toast;
 import net.minecraft.client.toast.ToastManager;
@@ -9,14 +10,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+@Environment(EnvType.CLIENT)
 @Mixin(ToastManager.class)
 public class ToastManagerMixin {
 
-    @Inject(method = "add", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "add(Lnet/minecraft/client/toast/Toast;)V", at = @At("HEAD"), cancellable = true)
     private void legacychat$add(Toast toast, CallbackInfo ci) {
         if (toast.getType() == SystemToast.Type.UNSECURE_SERVER_WARNING) {
             ci.cancel();
-            LegacyChatMod.LOGGER.info("legacychat$add unsecure server warning cancelled");
         }
     }
 }
